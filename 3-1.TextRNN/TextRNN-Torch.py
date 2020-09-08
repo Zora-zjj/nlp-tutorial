8133728e032e56ff2fd52b239f3dafc258d9eadb
Zora-zjj <54018191+Zora-zjj@users.noreply.github.com>
2020-09-08 10:16:17 +0800

    Update TextRNN-Torch.py

--- 3-1.TextRNN/TextRNN-Torch.py
+++ 3-1.TextRNN/TextRNN-Torch.py
@@ -8,40 +8,40 @@ import torch.optim as optim
 from torch.autograd import Variable
 
 dtype = torch.FloatTensor
+"""下面是处理输入数据"""
+# sentences = [ "i like dog", "i love coffee", "i hate milk"]
 
-sentences = [ "i like dog", "i love coffee", "i hate milk"]
+# word_list = " ".join(sentences).split()                 # ['i', 'like', 'dog', 'i', 'love', 'coffee', 'i', 'hate', 'milk']
+# word_list = list(set(word_list))                        # ['hate', 'milk', 'like', 'love', 'dog', 'coffee', 'i']
+# word_dict = {w: i for i, w in enumerate(word_list)}     # {'love': 0, 'like': 1, 'i': 2, 'hate': 3, 'coffee': 4, 'milk': 5, 'dog': 6}
+# number_dict = {i: w for i, w in enumerate(word_list)}   # {0: 'love', 1: 'like', 2: 'i', 3: 'hate', 4: 'coffee', 5: 'milk', 6: 'dog'}
+# n_class = len(word_dict)                                # 7
+ 
+# # TextRNN Parameter
+# batch_size = len(sentences)
+# n_step = 2 # number of cells(= number of Step)
+# n_hidden = 5 # number of hidden units in one cell
 
-word_list = " ".join(sentences).split()
-word_list = list(set(word_list))
-word_dict = {w: i for i, w in enumerate(word_list)}
-number_dict = {i: w for i, w in enumerate(word_list)}
-n_class = len(word_dict)
+# def make_batch(sentences):
+#     input_batch = []
+#     target_batch = []
 
-# TextRNN Parameter
-batch_size = len(sentences)
-n_step = 2 # number of cells(= number of Step)
-n_hidden = 5 # number of hidden units in one cell
+#     for sen in sentences:
+#         word = sen.split()
+#         input = [word_dict[n] for n in word[:-1]]
+#         target = word_dict[word[-1]]
 
-def make_batch(sentences):
-    input_batch = []
-    target_batch = []
+#         input_batch.append(np.eye(n_class)[input])
+#         target_batch.append(target)
 
-    for sen in sentences:
-        word = sen.split()
-        input = [word_dict[n] for n in word[:-1]]
-        target = word_dict[word[-1]]
-
-        input_batch.append(np.eye(n_class)[input])
-        target_batch.append(target)
-
-    return input_batch, target_batch
+#     return input_batch, target_batch
 
 # to Torch.Tensor
-input_batch, target_batch = make_batch(sentences)
-input_batch = Variable(torch.Tensor(input_batch))
-target_batch = Variable(torch.LongTensor(target_batch))
+input_batch, target_batch = make_batch(sentences)  
+input_batch = Variable(torch.Tensor(input_batch))        # [3,2,7] 数据都是01    [ "i like ", "i love ", "i hate "] 用onehot形式输入
+target_batch = Variable(torch.LongTensor(target_batch))  # tensor([6, 4, 5])     下一个词是[dog,coffee,milk]---->[6, 4, 5]    
 
-class TextRNN(nn.Module):
+class TextRNN(nn.Module):     # 预测句子下一个单词，分类：在单词表里取单词的下标
     def __init__(self):
         super(TextRNN, self).__init__()
 
@@ -49,12 +49,12 @@ class TextRNN(nn.Module):
         self.W = nn.Parameter(torch.randn([n_hidden, n_class]).type(dtype))
         self.b = nn.Parameter(torch.randn([n_class]).type(dtype))
 
-    def forward(self, hidden, X):
+    def forward(self, hidden, X):                  # output = model(hidden, input_batch)
         X = X.transpose(0, 1) # X : [n_step, batch_size, n_class]
         outputs, hidden = self.rnn(X, hidden)
         # outputs : [n_step, batch_size, num_directions(=1) * n_hidden]
         # hidden : [num_layers(=1) * num_directions(=1), batch_size, n_hidden]
-        outputs = outputs[-1] # [batch_size, num_directions(=1) * n_hidden]
+        outputs = outputs[-1] # [batch_size, num_directions(=1) * n_hidden]             # 例如 outputs：torch.Size([4, 5, 6])     outputs[-1] ：torch.Size([5, 6])   指取第一维度的倒一个
         model = torch.mm(outputs, self.W) + self.b # model : [batch_size, n_class]
         return model
 
